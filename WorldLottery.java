b7e0d45d8119823b7fcb40d3182c3d3c8a4682ec
Francis Wethington <fwethington3@gatech.edu>
2021-05-12 12:33:10 -0400

    Creating the Jframe

--- WorldLottery.java
+++ WorldLottery.java
@@ -1,3 +1,17 @@
-public static void main(string args[]){
-	
+import javax.swing.*;
+
+public class WorldLottery {
+
+	public static void main(String args[]){
+	JFrame frame = new JFrame("Test");
+	frame.setVisible(true);
+	frame.setSize(200,200);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
 }
+
+}
+
+
+
