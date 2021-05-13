716bfa79c7d17042621fcbb74113a2a8afe168fb
Francis Wethington <fwethington3@gatech.edu>
2021-05-13 10:54:11 -0400

    newBranch first commit

--- WorldLottery.java
+++ WorldLottery.java
@@ -1,7 +1,7 @@
-import javax.swing.*;
+qimport javax.swing.*;
 
 public class WorldLottery {
-//comment associated with the main branch
+
 	public static void main(String args[]){
 	JFrame frame = new JFrame("Test");
 	frame.setVisible(true);
@@ -9,23 +9,16 @@ public class WorldLottery {
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
-	JLabel labelTwo = new JLabel("hello again");
-	JPanel panelTwo = new JPanel();
+
 	JLabel label = new JLabel("hello");
 	JPanel panel = new JPanel();
 	frame.add(panel);
 	panel.add(label);
 
-	frame.add(panelTwo);
-	panelTwo.add(labelTwo);
 
-
-
-	JButton button = new JButton("hello again");
+	JButton button = new JButton("new Branch");
 
 	panel.add(button);
-	//adding some little comment
-	//The comment that is asociatted with branchingoffbranch
 
 
 }
